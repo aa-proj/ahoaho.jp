12506999fa4b808a32a33ccaeed96c1094a49c0f
takuwann2gou <takuwann2gou@users.noreply.github.com>
2021-02-18 13:52:25 +0000

    deploy: 0e7b68bb55a08096670d2aef538e05cd7f60b335

---
+++ _nuxt/f320b43.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{225:function(n,r,t){"use strict";t.r(r);var e={},w=t(27),component=Object(w.a)(e,(function(){var n=this,r=n.$createElement,t=n._self._c||r;return t("div",[t("nuxt-link",{attrs:{to:"/"}},[n._v("homeにもどる")]),n._v(" "),t("p",[n._v("this is unowrwrwrwrwrwrwr")])],1)}),[],!1,null,null,null);r.default=component.exports}}]);
