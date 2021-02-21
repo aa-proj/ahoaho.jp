a23d0fd0405dcabef001cee8bf6e42e96b0b6e32
takuwann2gou <tku1207@gmail.com>
2021-02-22 07:22:43 +0900

    updata home
    ちょいいけめんにした

--- components/UserButton.vue
+++ components/UserButton.vue
@@ -5,8 +5,9 @@
     </div>
     <div class="button-box" @click="$router.push(item.url)">
       <nuxt-link class="button-box-link" :to="item.url">
-        <p class="button-user-name">{{ item.name }}</p></nuxt-link
-      >
+        <p class="button-user-name">{{ item.name }}</p>
+        <p class="button-user-about">一週間水だけで生きれます。試しました。</p>
+      </nuxt-link>
     </div>
   </div>
 </template>
@@ -20,10 +21,12 @@ export default {
 
 <style scoped lang="scss">
 .user-button {
-  width: 700px;
+  width: 1000px;
   text-decoration: none;
   margin: auto;
-  margin-bottom: 60px;
+  padding-top: 100px;
+  margin-bottom: 200px;
+  margin-left: 150px;
   display: flex;
   justify-content: center;
 }
@@ -33,7 +36,10 @@ export default {
   height: 200px;
   background-color: rgba(240, 240, 255, 0);
   margin: 20px;
-  // box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.5);
+}
+
+img {
+  filter: drop-shadow(5px 5px 2px rgba(0, 0, 0, 0.5));
 }
 
 .user-image img {
@@ -43,19 +49,32 @@ export default {
 }
 
 .button-user-name {
-  font-size: 60px;
+  font-size: 70px;
   text-align: left;
-  color: rgb(0, 140, 255);
-  padding-left: 20px;
+  color: rgb(255, 255, 255);
+  padding-left: 30px;
+}
+.button-user-about {
+  font-size: 20px;
+  text-align: left;
+  color: rgb(255, 255, 255);
+  padding-left: 100px;
+  padding-top: 20px;
 }
 
 .button-box {
-  width: 400px;
+  width: 800px;
   height: 200px;
-  background-color: rgb(240, 240, 255);
+  border-right: solid;
+  border-width: 5px;
+  border-color: rgb(255, 255, 255);
+  //border: groove;
+  background-color: rgba(240, 240, 255, 0);
   margin-top: 20px;
   margin-bottom: 20px;
-  box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.5);
+  margin-left: 100px;
+  padding-right: 100px;
+  box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0);
 }
 
 .button-box-link {
